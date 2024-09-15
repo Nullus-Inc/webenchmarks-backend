048b4cf887ca58e6ce4f897585d9ff89c3ec8ccc
Alexandre Duteau <alex@midaslabs.co>
2024-09-14 20:56:15 -0600

    Updated User

--- users.go
+++ users.go
@@ -20,7 +20,7 @@ func CreateUser(ctx context.Context, p *User) (*User, error) {
 
 	// Create User
 	_, err := db.Exec(ctx,
-		`INSERT INTO "user" (username, first_name, last_name, dob, email, password)
+		`INSERT INTO user (username, first_name, last_name, dob, email, password)
 		VALUES ($1, $2, $3, $4, $5, $6)`,
 		p.Username, p.FirstName, p.LastName, p.DOB, p.Email, p.Password)
 	if err != nil {
@@ -39,7 +39,7 @@ func UpdateUser(ctx context.Context, p *User) (*User, error) {
 
 	// Update User
 	_, err := db.Exec(ctx,
-		`UPDATE "user"
+		`UPDATE user
 		SET first_name = $1, last_name = $2, dob = $3, email = $4, password = $5
 		WHERE username = $6`,
 		p.FirstName, p.LastName, p.DOB, p.Email, p.Password, p.Username)
@@ -60,7 +60,7 @@ func GetUser(ctx context.Context, username string) (*User, error) {
 	// Get User
 	user := &User{Username: username}
 	err := db.QueryRow(ctx,
-		`SELECT first_name, last_name, dob, email, password FROM "user" WHERE username = $1`,
+		`SELECT first_name, last_name, dob, email, password FROM user WHERE username = $1`,
 		username).Scan(&user.FirstName, &user.LastName, &user.DOB, &user.Email, &user.Password)
 	if err != nil {
 		return nil, err
@@ -78,7 +78,7 @@ func DeleteUser(ctx context.Context, username string) error {
 
 	// Delete User
 	_, err := db.Exec(ctx,
-		`DELETE FROM "user" WHERE username = $1`, username)
+		`DELETE FROM user WHERE username = $1`, username)
 	if err != nil {
 		return err
 	}
